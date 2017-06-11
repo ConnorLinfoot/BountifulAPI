3651430cb400fd231e72b1e1c848ce225f375604
Connor Linfoot <connorlinfoot@me.com>
2017-06-11 20:31:02 +0100

    1.12 Support

--- src/main/java/com/connorlinfoot/bountifulapi/BountifulAPI.java
+++ src/main/java/com/connorlinfoot/bountifulapi/BountifulAPI.java
@@ -136,11 +136,14 @@ public class BountifulAPI extends JavaPlugin implements Listener {
 		try {
 			Object tabHeader = getNMSClass("IChatBaseComponent").getDeclaredClasses()[0].getMethod("a", String.class).invoke(null, "{\"text\":\"" + header + "\"}");
 			Object tabFooter = getNMSClass("IChatBaseComponent").getDeclaredClasses()[0].getMethod("a", String.class).invoke(null, "{\"text\":\"" + footer + "\"}");
-			Constructor<?> titleConstructor = getNMSClass("PacketPlayOutPlayerListHeaderFooter").getConstructor(getNMSClass("IChatBaseComponent"));
-			Object packet = titleConstructor.newInstance(tabHeader);
-			Field field = packet.getClass().getDeclaredField("b");
-			field.setAccessible(true);
-			field.set(packet, tabFooter);
+			Constructor<?> titleConstructor = getNMSClass("PacketPlayOutPlayerListHeaderFooter").getConstructor();
+			Object packet = titleConstructor.newInstance();
+			Field aField = packet.getClass().getDeclaredField("a");
+			aField.setAccessible(true);
+			aField.set(packet, tabHeader);
+			Field bField = packet.getClass().getDeclaredField("b");
+			bField.setAccessible(true);
+			bField.set(packet, tabFooter);
 			sendPacket(player, packet);
 		} catch (Exception ex) {
 			ex.printStackTrace();
@@ -148,34 +151,84 @@ public class BountifulAPI extends JavaPlugin implements Listener {
 	}
 
 	public static void sendActionBar(Player player, String message) {
+		if (!player.isOnline()) {
+			return; // Player may have logged out
+		}
+		// Call the event, if cancelled don't send Action Bar
 		ActionBarMessageEvent actionBarMessageEvent = new ActionBarMessageEvent(player, message);
 		Bukkit.getPluginManager().callEvent(actionBarMessageEvent);
 		if (actionBarMessageEvent.isCancelled())
 			return;
 
+		if (nmsver.startsWith("v1_12_")) {
+			sendActionBarPost112(player, message);
+		} else {
+			sendActionBarPre112(player, message);
+		}
+	}
+
+	private static void sendActionBarPost112(Player player, String message) {
+		if (!player.isOnline()) {
+			return; // Player may have logged out
+		}
+
 		try {
-			Class<?> c1 = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftPlayer");
-			Object p = c1.cast(player);
+			Class<?> craftPlayerClass = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftPlayer");
+			Object craftPlayer = craftPlayerClass.cast(player);
+			Object ppoc;
+			Class<?> c4 = Class.forName("net.minecraft.server." + nmsver + ".PacketPlayOutChat");
+			Class<?> c5 = Class.forName("net.minecraft.server." + nmsver + ".Packet");
+			Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatComponentText");
+			Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
+			Class<?> chatMessageTypeClass = Class.forName("net.minecraft.server." + nmsver + ".ChatMessageType");
+			Object[] chatMessageTypes = chatMessageTypeClass.getEnumConstants();
+			Object chatMessageType = null;
+			for (Object obj : chatMessageTypes) {
+				if (obj.toString().equals("GAME_INFO")) {
+					chatMessageType = obj;
+				}
+			}
+			Object o = c2.getConstructor(new Class<?>[]{String.class}).newInstance(message);
+			ppoc = c4.getConstructor(new Class<?>[]{c3, chatMessageTypeClass}).newInstance(o, chatMessageType);
+			Method m1 = craftPlayerClass.getDeclaredMethod("getHandle");
+			Object h = m1.invoke(craftPlayer);
+			Field f1 = h.getClass().getDeclaredField("playerConnection");
+			Object pc = f1.get(h);
+			Method m5 = pc.getClass().getDeclaredMethod("sendPacket", c5);
+			m5.invoke(pc, ppoc);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private static void sendActionBarPre112(Player player, String message) {
+		if (!player.isOnline()) {
+			return; // Player may have logged out
+		}
+
+		try {
+			Class<?> craftPlayerClass = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftPlayer");
+			Object craftPlayer = craftPlayerClass.cast(player);
 			Object ppoc;
 			Class<?> c4 = Class.forName("net.minecraft.server." + nmsver + ".PacketPlayOutChat");
 			Class<?> c5 = Class.forName("net.minecraft.server." + nmsver + ".Packet");
 			if (useOldMethods) {
 				Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatSerializer");
 				Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
-				Method m3 = c2.getDeclaredMethod("a", new Class<?>[]{String.class});
+				Method m3 = c2.getDeclaredMethod("a", String.class);
 				Object cbc = c3.cast(m3.invoke(c2, "{\"text\": \"" + message + "\"}"));
-				ppoc = c4.getConstructor(new Class<?>[]{c3, byte.class}).newInstance(new Object[]{cbc, (byte) 2});
+				ppoc = c4.getConstructor(new Class<?>[]{c3, byte.class}).newInstance(cbc, (byte) 2);
 			} else {
 				Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatComponentText");
 				Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
-				Object o = c2.getConstructor(new Class<?>[]{String.class}).newInstance(new Object[]{message});
-				ppoc = c4.getConstructor(new Class<?>[]{c3, byte.class}).newInstance(new Object[]{o, (byte) 2});
+				Object o = c2.getConstructor(new Class<?>[]{String.class}).newInstance(message);
+				ppoc = c4.getConstructor(new Class<?>[]{c3, byte.class}).newInstance(o, (byte) 2);
 			}
-			Method m1 = c1.getDeclaredMethod("getHandle", new Class<?>[]{});
-			Object h = m1.invoke(p);
+			Method m1 = craftPlayerClass.getDeclaredMethod("getHandle");
+			Object h = m1.invoke(craftPlayer);
 			Field f1 = h.getClass().getDeclaredField("playerConnection");
 			Object pc = f1.get(h);
-			Method m5 = pc.getClass().getDeclaredMethod("sendPacket", new Class<?>[]{c5});
+			Method m5 = pc.getClass().getDeclaredMethod("sendPacket", c5);
 			m5.invoke(pc, ppoc);
 		} catch (Exception ex) {
 			ex.printStackTrace();
